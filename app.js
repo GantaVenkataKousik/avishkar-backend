a4f1fa49a487619007194b4ec2efb88c2bd76538
GVK <venkatakousikcse01@gmail.com>
2025-03-28 23:27:33 +0530

    feat: adding project

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import courseRoutes from './routes/courseRoutes.js';
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/v1/course', courseRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: 'Something went wrong!'
+  });
+});
+
+export default app; 
